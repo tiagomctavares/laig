6b92c45764165282a1ea85460d5e5fcef2661325
tiago <tiagomctavares@gmail.com>
2015-12-10 16:57:01 +0000

    changed prolog server location

---
+++ Projecto_3/reader/logic/Game.js
@@ -0,0 +1,12 @@
+function Game() {
+
+    // MyPrimitive.call(this, scene);
+
+}
+
+// MyCylinder.prototype = Object.create(MyPrimitive.prototype);
+// MyCylinder.prototype.constructor = MyCylinder;
+
+//MyCylinder.prototype.initBuffers = function() {
+
+//};
